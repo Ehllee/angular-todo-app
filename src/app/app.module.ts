f6a4cfa8875e5b48e315971acb1a802fa7c6cefe
An Phan <anxiety.an03@gmail.com>
2023-08-05 21:07:32 +0700

    Clear old look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
-import { DoneTodoListComponent } from './todo-list/done-todo-list/done-todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
-    DoneTodoListComponent,
   ],
   imports: [
     BrowserModule,
